cfd76f27803ccecdc98f9ba174f0f7f605aa5309
gabrielaRazo <gabrielarazoroldan@gmail.com>
2020-02-16 21:48:35 -0600

    I added new classes.
    I added a constructor, a UX class, and a DOM event to save the data on the forms.

---
+++ App.js
@@ -0,0 +1,20 @@
+class Product {
+  constructor(name, price, year) {
+    this.name = name;
+    this.price = price;
+    this.year = year;
+  }
+}
+class UI {
+  addProduct() {}
+  deleteProduct() {}
+  showMessage() {}
+}
+//DOM Events
+document.getElementById("product-form").addEventListener("submit", function(e) {
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const year = document.getElementById("year").value;
+  const product = new Product(name, price, year);
+  e.preventDefault();
+});
